19456e682dacbbf08078cb5a8a4d8458381d33cf
Alexander White <alex@picotory.com>
2021-01-20 23:40:36 +0000

    Intro BS

--- app/tpl/index.php
+++ app/tpl/index.php
@@ -1,3 +1,19 @@
-<?php
-	echo 'Hello world';
-?>
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Bootstrap CSS -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+		<title>Hello, world!</title>
+	</head>
+	<body>
+		<h1>Hello, world!</h1>
+
+		<!-- Bootstrap Bundle with Popper -->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+	</body>
+</html>
