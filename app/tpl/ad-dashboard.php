3e111c25488b08a81eb3330b05d5f52ee3eaf398
Alex White <alex@picotory.com>
2021-01-21 12:03:05 +0000

    Add single br

--- app/tpl/ad-dashboard.php
+++ app/tpl/ad-dashboard.php
@@ -82,6 +82,7 @@
 						<!-- Intro to the dashboard -->
 						<br />
 						<h4 class="card-title">Welcome to your Dashboard</h4>
+						<br />
 						<p class="card-text">This is Storio!</p>
 						<!--
 						<div class="row">
