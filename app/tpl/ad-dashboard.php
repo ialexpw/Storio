740baa81c8a5443c69a7e36a38afe2c9f5ddee5c
Alex White <alex@picotory.com>
2021-01-21 11:06:04 +0000

    Update links

--- app/tpl/ad-dashboard.php
+++ app/tpl/ad-dashboard.php
@@ -1,4 +1,114 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<title>Storio</title>
+
+		<link rel="canonical" href="https://storio.aw0.uk">
+
+		<!-- Bootstrap core CSS -->
+		<link rel="stylesheet" href="app/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+		<style>
+			
+		</style>
 
 
+		<!-- Custom styles -->
+		<link href="app/css/custom.css" rel="stylesheet">
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">Navbar</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div class="collapse navbar-collapse" id="navbarsExampleDefault">
+					<ul class="navbar-nav me-auto mb-2 mb-md-0">
+						<li class="nav-item active">
+							<a class="nav-link" aria-current="page" href="#">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Link</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+							<ul class="dropdown-menu" aria-labelledby="dropdown01">
+								<li><a class="dropdown-item" href="#">Action</a></li>
+								<li><a class="dropdown-item" href="#">Another action</a></li>
+								<li><a class="dropdown-item" href="#">Something else here</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+
+		<main class="container">
+			<div class="starter-template text-center py-5 px-3">
+				<div class="card text-center">
+					<div class="card-header">
+						<ul class="nav nav-tabs card-header-tabs">
+							<li class="nav-item" style="width:12%;">
+								<a class="nav-link active" aria-current="true" href="?page=ad-dashboard.php">
+									<i class="bi bi-house" style="font-size: 2.2rem;"></i>
+									<br />Dashboard
+								</a>
+							</li>
+							<li class="nav-item" style="width:12%;">
+								<a class="nav-link" href="?page=ad-filemanager.php">
+									<i class="bi bi-files" style="font-size: 2rem; color: cornflowerblue;"></i>
+									<br />File Manager
+								</a>
+							</li>
+							<li class="nav-item" style="width:12%;">
+								<a class="nav-link" href="?page=ad-users.php">
+									<i class="bi bi-people" style="font-size: 2rem; color: cornflowerblue;"></i>
+									<br />Users
+								</a>
+							</li>
+							<li class="nav-item" style="width:12%;">
+								<a class="nav-link" href="?page=ad-messages.php">
+									<i class="bi bi-chat" style="font-size: 2rem; color: cornflowerblue;"></i>
+									<br />Messages
+								</a>
+							</li>
+							<li class="nav-item" style="width:12%;">
+								<a class="nav-link" href="?page=ad-logs.php">
+									<i class="bi bi-archive" style="font-size: 2rem; color: cornflowerblue;"></i>
+									<br />Logs
+								</a>
+							</li>
+							<li class="nav-item" style="width:12%;">
+								<a class="nav-link" href="?page=ad-settings.php">
+									<i class="bi bi-gear" style="font-size: 2rem; color: cornflowerblue;"></i>
+									<br />Settings
+								</a>
+							</li>
+							<!--<li class="nav-item">
+								<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+							</li>-->
+						</ul>
+					</div>
+					<div class="card-body">
+						<h5 class="card-title">Special title treatment</h5>
+						<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+						<a href="#" class="btn btn-primary">Go somewhere</a>
+					</div>
+				</div>
+			</div>
+		</main>
 
 
+		<script src="app/js/bootstrap.bundle.min.js"></script>
+	</body>
+</html>
