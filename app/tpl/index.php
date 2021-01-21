087887f07d65ceb51db5b110ce407d0c4d3c4d49
Alex White <alex@picotory.com>
2021-01-21 11:03:34 +0000

    Alter widths

--- app/tpl/index.php
+++ app/tpl/index.php
@@ -58,37 +58,37 @@
 				<div class="card text-center">
 					<div class="card-header">
 						<ul class="nav nav-tabs card-header-tabs">
-							<li class="nav-item">
+							<li class="nav-item" style="width:12%;">
 								<a class="nav-link active" aria-current="true" href="#">
 									<i class="bi bi-house" style="font-size: 2.2rem;"></i>
 									<br />Dashboard
 								</a>
 							</li>
-							<li class="nav-item" style="width:15%;">
+							<li class="nav-item" style="width:12%;">
 								<a class="nav-link" href="#">
 									<i class="bi bi-files" style="font-size: 2rem; color: cornflowerblue;"></i>
 									<br />File Manager
 								</a>
 							</li>
-							<li class="nav-item">
+							<li class="nav-item" style="width:12%;">
 								<a class="nav-link" href="#">
 									<i class="bi bi-people" style="font-size: 2rem; color: cornflowerblue;"></i>
 									<br />Users
 								</a>
 							</li>
-							<li class="nav-item">
+							<li class="nav-item" style="width:12%;">
 								<a class="nav-link" href="#">
 									<i class="bi bi-chat" style="font-size: 2rem; color: cornflowerblue;"></i>
 									<br />Messages
 								</a>
 							</li>
-							<li class="nav-item">
+							<li class="nav-item" style="width:12%;">
 								<a class="nav-link" href="#">
 									<i class="bi bi-archive" style="font-size: 2rem; color: cornflowerblue;"></i>
 									<br />Logs
 								</a>
 							</li>
-							<li class="nav-item">
+							<li class="nav-item" style="width:12%;">
 								<a class="nav-link" href="#">
 									<i class="bi bi-gear" style="font-size: 2rem; color: cornflowerblue;"></i>
 									<br />Settings
