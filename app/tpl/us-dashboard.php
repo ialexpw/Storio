be2c0f1d8ae9448518eec8edb95e385e18a1852e
Alexander White <alex@picotory.com>
2021-01-21 21:19:31 +0000

    Update user nav

--- app/tpl/us-dashboard.php
+++ app/tpl/us-dashboard.php
@@ -74,24 +74,6 @@
 								</a>
 							</li>
 							<li class="nav-item" style="width:12%;">
-								<a class="nav-link" href="?page=ad-users">
-									<i class="bi bi-people" style="font-size: 2rem; color: cornflowerblue;"></i>
-									<br />Users
-								</a>
-							</li>
-							<li class="nav-item" style="width:12%;">
-								<a class="nav-link" href="?page=ad-messages">
-									<i class="bi bi-chat" style="font-size: 2rem; color: cornflowerblue;"></i>
-									<br />Messages
-								</a>
-							</li>
-							<li class="nav-item" style="width:12%;">
-								<a class="nav-link" href="?page=ad-logs">
-									<i class="bi bi-archive" style="font-size: 2rem; color: cornflowerblue;"></i>
-									<br />Logs
-								</a>
-							</li>
-							<li class="nav-item" style="width:12%;">
 								<a class="nav-link" href="?page=ad-settings">
 									<i class="bi bi-gear" style="font-size: 2rem; color: cornflowerblue;"></i>
 									<br />Settings
