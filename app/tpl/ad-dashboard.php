f56c407c3f750f0bcecd250bddd3b3aee4acd1d2
Alex White <alex@picotory.com>
2021-01-21 11:51:35 +0000

    Update design

--- app/tpl/ad-dashboard.php
+++ app/tpl/ad-dashboard.php
@@ -27,7 +27,7 @@
 	<body>
 		<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
 			<div class="container-fluid">
-				<i class="bi bi-droplet" style="margin-right:12px; color: cornflowerblue;"></i>
+				<i class="bi bi-droplet" style="font-size: 2rem; margin-right:12px; margin-bottom:6px; color: cornflowerblue;"></i>
 				<a class="navbar-brand" href="?page=ad-dashboard"> Storio File Management</a>
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
