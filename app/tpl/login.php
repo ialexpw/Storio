bacf5b73d6838233a129f35f69b43a9b10490b60
Alexander White <alex@picotory.com>
2021-01-21 20:38:42 +0000

    Log in form

--- app/tpl/login.php
+++ app/tpl/login.php
@@ -69,22 +69,17 @@
 						<br />
 						<h4 class="card-title">Log in to Storio</h4>
 						<p class="card-text" style="margin-top:15px;">
-<form>
-<div class="mb-3">
-<label for="exampleInputEmail1" class="form-label">Email address</label>
-<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-</div>
-<div class="mb-3">
-<label for="exampleInputPassword1" class="form-label">Password</label>
-<input type="password" class="form-control" id="exampleInputPassword1">
-</div>
-<div class="mb-3 form-check">
-<input type="checkbox" class="form-check-input" id="exampleCheck1">
-<label class="form-check-label" for="exampleCheck1">Check me out</label>
-</div>
-<button type="submit" class="btn btn-primary">Submit</button>
-</form>
+							<form>
+								<div class="mb-3">
+									<label for="userInput" class="form-label">User name</label>
+									<input type="text" class="form-control" id="userInput" name="userInput">
+								</div>
+								<div class="mb-3">
+									<label for="passInput" class="form-label">Password</label>
+									<input type="password" class="form-control" id="passInput" name="passInput">
+								</div>
+								<button type="submit" class="btn btn-primary">Log in</button>
+							</form>
 						</p>
 					</div>
 				</div>
