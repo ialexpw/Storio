b18cb6fdc5f9648e22a59a03c99f00c6501a7f9a
Alex White <alex@picotory.com>
2021-01-21 11:18:06 +0000

    Change nav

--- app/tpl/ad-dashboard.php
+++ app/tpl/ad-dashboard.php
@@ -24,11 +24,12 @@
 	<body>
 		<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
 			<div class="container-fluid">
-				<a class="navbar-brand" href="#">Navbar</a>
+				<a class="navbar-brand" href="#">Storio File Management</a>
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 
+				<!--
 				<div class="collapse navbar-collapse" id="navbarsExampleDefault">
 					<ul class="navbar-nav me-auto mb-2 mb-md-0">
 						<li class="nav-item active">
@@ -50,6 +51,7 @@
 						</li>
 					</ul>
 				</div>
+				-->
 			</div>
 		</nav>
 
@@ -99,6 +101,7 @@
 					<div class="card-body">
 						<!-- Site stats -->
 						<h4 class="card-title text-left">System Statistics</h4>
+						<br />
 						<div class="row">
 							<div class="col-sm">
 								<h5 class="card-title">Today</h5>
@@ -118,6 +121,7 @@
 
 						<!-- System stats -->
 						<h4 class="card-title text-left">System Information</h4>
+						<br />
 						<div class="row">
 							<div class="col-sm">
 								<h5 class="card-title">Today</h5>
