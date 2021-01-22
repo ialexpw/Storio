7d3127648c5d35fa2f26120bb4550aa47edb3c33
Alex White <alex@picotory.com>
2021-01-22 14:37:36 +0000

    rm

--- app/tpl/us-files.php
+++ app/tpl/us-files.php
@@ -110,11 +110,6 @@
 						<h4 class="card-title">File Management</h4>
 						<p class="card-text" style="margin-top:15px;">
 							<?php
-								echo 'User dir: ' . $usrDir . '<br />';
-								echo 'Browse dir: ' . $getBrowse . '<br />';
-
-								$browseDir = $usrDir . $getBrowse;
-
 								// Save the arrays
 								$fldArr = Storio::DirList($usrDir . $getBrowse);
 
@@ -167,105 +162,6 @@
 
 								// End the row
 								echo '</div>';
-
-								//echo '<pre>';
-								//print_r($fldArr);
-								//echo '</pre>';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-							/*
-								echo 'User dir: ' . $usrDir . '<br />';
-								echo 'Browse: ' . $getBrowse . '<br />';
-
-								// Save the arrays
-								$fldArr = Storio::DirList($usrDir . $getBrowse);
-
-								echo 'Browsing dir: ' . $usrDir . $getBrowse . '<br />';
-
-								// Use for the path
-								$strPath = $usrDir . $getBrowse;
-								//$strPath = $usrDir . substr($getBrowse, 0, -1);
-
-								// Use for str_replace
-								//$strRep = $usrDir . $getBrowse;
-
-								echo 'strPath: ' . $strPath . '<br />';
-								//echo 'strRep: ' . $strRep . '<br />';
-
-								echo '<pre>';
-								print_r($fldArr);
-								echo '</pre>';
-
-								// Start the row
-								echo '<div class="row">';
-
-								echo '<div class="col-md-4"><b>File name</b></div>';
-								echo '<div class="col-md-4"><b>Size</b></div>';
-								echo '<div class="col-md-4"><b>Actions</b></div>';
-								echo '<hr>';
-
-								// Check if there are subfolders first (avoid warnings)
-								if(!empty($fldArr['dirview'][$strPath]['folders'])) {
-									// Loop the folders first
-									foreach($fldArr['dirview'][$strPath]['folders'] as $dir) {
-										// Replace the beginning of the path
-										$dir = str_replace($strPath.'/', "", $dir);
-
-										// Generate a link to subfolder
-										$subLink = $getBrowse . $dir . '/';
-
-										// Folder icon
-										$foldIco = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder" viewBox="0 0 16 16">
-										<path d="M.54 3.87L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z"/>
-										</svg>';
-
-										echo '<div class="col-md-4">' . $foldIco . ' <a href="?page=us-files&browse=' . $subLink . '">' . $dir . '</a></div>';
-										echo '<div class="col-md-4">n/a</div>';
-										echo '<div class="col-md-4">n/a</div>';
-									}
-								}
-								
-								// Check if there are files first (avoid warnings)
-								if(!empty($fldArr['dirview'][$strPath]['files'])) {
-									// Loop the files after
-									foreach($fldArr['dirview'][$strPath]['files'] as $file) {
-										// Replace the beginning of the path
-										$file = str_replace($strPath.'/', "", $file);
-
-										// File icon
-										$fileIco = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-text" viewBox="0 0 16 16">
-										<path d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1H5z"/>
-										<path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z"/>
-										</svg>';
-
-										echo '<div class="col-md-4">' . $fileIco . ' ' . $file . '</div>';
-										echo '<div class="col-md-4">' . Storio::ReadableSize(filesize($strRep . $getBrowse . $file)) . '</div>';
-										echo '<div class="col-md-4">Download - Delete</div>';
-									}
-								}
-								
-								// End the row
-								echo '</div>';
-
-							*/
 							?>
 						</p>
 					</div>
