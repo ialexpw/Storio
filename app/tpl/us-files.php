24c3ad046e086c4e141e0ddd8a864431dbfd41c4
Alex White <alex@picotory.com>
2021-01-22 14:34:45 +0000

    test

--- app/tpl/us-files.php
+++ app/tpl/us-files.php
@@ -120,8 +120,6 @@
 
 								if(!empty($fldArr['dirview'][$usrDir.$getBrowse]['folders'])) {
 									foreach($fldArr['dirview'][$usrDir.$getBrowse]['folders'] as $dir) {
-										//print_r($dir);
-
 										// Replace the beginning of the path
 										$dir = str_replace($usrDir.$getBrowse.'/', "", $dir);
 
@@ -143,9 +141,23 @@
 								if(!empty($fldArr['dirview'][$usrDir.$getBrowse]['files'])) {
 									// Loop the files after
 									foreach($fldArr['dirview'][$usrDir.$getBrowse]['files'] as $file) {
-										echo '<pre>';
-										print_r($file);
-										echo '</pre>';
+										// Replace the beginning of the path
+										$file = str_replace($usrDir.$getBrowse.'/', "", $file);
+
+										// File icon
+										$fileIco = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-text" viewBox="0 0 16 16">
+										<path d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1H5z"/>
+										<path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z"/>
+										</svg>';
+
+										echo '<div class="col-md-4">' . $fileIco . ' ' . $file . '</div>';
+										echo '<div class="col-md-4">' . Storio::ReadableSize(filesize($usrDir . $getBrowse. '/' . $file)) . '</div>';
+										echo '<div class="col-md-4">Download - Delete</div>';
+
+
+										//echo '<pre>';
+										//print_r($file);
+										//echo '</pre>';
 										/*
 										// Replace the beginning of the path
 										$file = str_replace($strRep, "", $file);
