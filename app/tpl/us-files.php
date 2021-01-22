fe3d00d889b9f4849e6a4f8217a0b2c86bcf560a
Alex White <alex@picotory.com>
2021-01-22 10:17:55 +0000

    full path

--- app/tpl/us-files.php
+++ app/tpl/us-files.php
@@ -118,15 +118,11 @@
 
 								echo 'Browsing dir: ' . $usrDir . '/' . $getBrowse . '<br />';
 
-
-
-
-
 								// Use for the path
-								$strPath = $usrDir;
+								$strPath = $usrDir . '/' . $getBrowse;
 
 								// Use for str_replace
-								$strRep = $usrDir . '/';
+								$strRep = $usrDir . '/' . $getBrowse . '/';
 
 								echo '<pre>';
 								print_r($fldArr);
