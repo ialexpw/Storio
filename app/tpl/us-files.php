604e54475a6f6397845f985ca2d3333af1c19197
Alexander White <alex@picotory.com>
2021-01-23 15:23:51 +0000

    trial

--- app/tpl/us-files.php
+++ app/tpl/us-files.php
@@ -116,9 +116,10 @@
 								// Start the row
 								echo '<div class="row">';
 
-								echo '<div class="col-md-4"><b>File name</b></div>';
-								echo '<div class="col-md-4"><b>Size</b></div>';
-								echo '<div class="col-md-4"><b>Actions</b></div>';
+								echo '<div class="col-md-3"><b>File name</b></div>';
+								echo '<div class="col-md-3"><b>Type</b></div>';
+								echo '<div class="col-md-3"><b>Size</b></div>';
+								echo '<div class="col-md-3"><b>Actions</b></div>';
 								echo '<hr>';
 
 								// Check if there are subfolders first (avoid warnings)
@@ -135,9 +136,9 @@
 										<path d="M.54 3.87L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z"/>
 										</svg>';
 
-										echo '<div class="col-md-4">' . $foldIco . ' <a href="?page=us-files&browse=' . ltrim($subLink, '/') . '">' . $dir . '</a></div>';
-										echo '<div class="col-md-4">n/a</div>';
-										echo '<div class="col-md-4">n/a</div>';
+										echo '<div class="col-md-12">' . $foldIco . ' <a href="?page=us-files&browse=' . ltrim($subLink, '/') . '">' . $dir . '</a></div>';
+										//echo '<div class="col-md-">n/a</div>';
+										//echo '<div class="col-md-">n/a</div>';
 									}
 								}
 
@@ -154,9 +155,10 @@
 										<path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z"/>
 										</svg>';
 
-										echo '<div class="col-md-4">' . $fileIco . ' ' . $file . '</div>';
-										echo '<div class="col-md-4">' . Storio::ReadableSize(filesize($usrDir . $getBrowse. '/' . $file)) . '</div>';
-										echo '<div class="col-md-4">Download - Delete</div>';
+										echo '<div class="col-md-3">' . $fileIco . ' ' . $file . '</div>';
+										echo '<div class="col-md-3">' . mime_content_type($usrDir . $getBrowse. '/' . $file) . '</div>';
+										echo '<div class="col-md-3">' . Storio::ReadableSize(filesize($usrDir . $getBrowse. '/' . $file)) . '</div>';
+										echo '<div class="col-md-3">Download - Delete</div>';
 									}
 								}
 
