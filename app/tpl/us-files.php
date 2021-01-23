278767b26c408e461a451f39cb6e59fe14f8dac9
Alexander White <alex@picotory.com>
2021-01-23 20:14:08 +0000

    sizing

--- app/tpl/us-files.php
+++ app/tpl/us-files.php
@@ -135,8 +135,8 @@
 
 								echo '<div class="col-md-6"><b>File name</b></div>';
 								echo '<div class="col-md-2"><b>Type</b></div>';
-								echo '<div class="col-md-2"><b>Size</b></div>';
-								echo '<div class="col-md-2"><b>Actions</b></div>';
+								echo '<div class="col-md-1"><b>Size</b></div>';
+								echo '<div class="col-md-3"><b>Actions</b></div>';
 								echo '<hr>';
 
 								// Show the back button if needed
@@ -168,8 +168,8 @@
 
 										echo '<div class="col-md-6">' . $foldIco . ' <a href="?page=us-files&browse=' . ltrim($subLink, '/') . '">' . $dir . '</a></div>';
 										echo '<div class="col-md-2">directory</div>';
-										echo '<div class="col-md-2">n/a</div>';
-										echo '<div class="col-md-2"><span style="color:red;">Delete</span></div>';
+										echo '<div class="col-md-1">n/a</div>';
+										echo '<div class="col-md-3"><span style="color:red;">Delete</span></div>';
 									}
 								}
 
