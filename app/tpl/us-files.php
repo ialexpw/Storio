db2737ea6ea16de463dd413db59b60c7efef37cd
Alexander White <alex@picotory.com>
2021-01-23 20:36:57 +0000

    test

--- app/tpl/us-files.php
+++ app/tpl/us-files.php
@@ -206,7 +206,7 @@
 										echo '<div class="col-md-6">' . $fileIco . ' ' . $file . '</div>';
 										echo '<div class="col-md-2" style="text-align:center;">' . mime_content_type($usrDir . $getBrowse. '/' . $file) . '</div>';
 										echo '<div class="col-md-2" style="text-align:center;">' . Storio::ReadableSize(filesize($usrDir . $getBrowse. '/' . $file)) . '</div>';
-										echo '<div class="col-md-2" style="text-align:center;"><span style="margin-right:22px;>' . $dlIco . '</span> <span style="margin-right:22px;>' . $copyIco . '</span> <span style="color:red; margin-right:2px;">' . $delIco . '</span></div>';
+										echo '<div class="col-md-2" style="text-align:center;"><span style="margin-right:22px;>' . $dlIco . '</span> <span style="margin-right:22px;>' . $copyIco . '</span> <span style="color:red; margin-right:2px;">' . $copyIco . '</span></div>';
 									}
 								}
 
