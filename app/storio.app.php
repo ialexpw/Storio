dcefc10703ce256e98516dbf9038ea81de99b440
Alexander White <alex@picotory.com>
2021-01-24 16:44:16 +0000

    ico

--- app/storio.app.php
+++ app/storio.app.php
@@ -409,7 +409,12 @@
 			<path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z"/>
 			</svg>';
 
+			// Compressed icon
 
+			// Default file
+			$defIco = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file" viewBox="0 0 16 16">
+			<path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
+			</svg>';
 
 			// Grab the extension
 			$ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
@@ -425,8 +430,10 @@
 				return $cIco;
 			}else if(in_array($ext, $audArr)) {
 				return $aIco;
-			}else if(in_array($ext, 'pdf')) {
+			}else if($ext == 'pdf') {
 				return $pdIco;
+			}else{
+				return $defIco;
 			}
 		}
 
