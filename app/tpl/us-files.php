522b9f12d73b0e661356e844edf9c93d954518c9
Alex White <alex@picotory.com>
2021-02-19 09:37:20 +0000

    Add number formatting

--- app/tpl/us-files.php
+++ app/tpl/us-files.php
@@ -294,7 +294,7 @@
 
 								echo '<br /><hr><p class="text-center">Storage allocation</p><div class="progress" style="border: 1px solid #000; width:75%;">';
 								echo '<div class="progress-bar" role="progressbar" style="color:black; width: ' . $percUsed . '%" aria-valuenow="' . $percUsed . '" aria-valuemin="0" aria-valuemax="100"></div>';
-								echo '<small class="justify-content-center d-flex position-absolute w-75">' . $usrCfg['usedStorage'] . 'MB / ' . $usrCfg['maxStorage'] . 'MB</small>';
+								echo '<small class="justify-content-center d-flex position-absolute w-75">' . number_format($usrCfg['usedStorage']) . 'MB / ' . number_format($usrCfg['maxStorage']) . 'MB</small>';
 								echo '</div>';
 							?>
 						</p>
