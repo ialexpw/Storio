518e16b26cea935f499da7dafe14553a90553dc4
Alex White <alex@picotory.com>
2021-02-19 11:18:31 +0000

    Add text

--- app/tpl/us-files.php
+++ app/tpl/us-files.php
@@ -440,7 +440,7 @@ Share link copied to clipboard.
 <div class="toast align-items-center bottom-0 end-0" role="alert" aria-live="assertive" aria-atomic="true">
 <div class="d-flex">
 <div class="toast-body">
-Hello, world! This is a toast message.
+Share link has been copied to your clipboard.
 </div>
 <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
 </div>
