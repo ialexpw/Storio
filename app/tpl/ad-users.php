c51455038d6715ff46fd6e54922047e3ebce68b1
Alex White <alex@picotory.com>
2021-02-19 09:34:04 +0000

    Form class

--- app/tpl/ad-users.php
+++ app/tpl/ad-users.php
@@ -191,7 +191,7 @@
 						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 					</div>
 					<div class="modal-body">
-						<form method="post" action="?page=ad-users">
+						<form class="row g-3" method="post" action="?page=ad-users">
 							<div class="row g-3">
 								<!-- Username -->
 								<div class="col-md">
