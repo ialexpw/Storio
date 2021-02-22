4cb7e80acbc08671b218e54bc52a893236224762
Alexander White <alex@picotory.com>
2021-02-22 23:15:17 +0000

    test

---
+++ wasabi.php
@@ -0,0 +1,17 @@
+<?php
+	// Include the SDK using the Composer autoloader
+	require 'vendor/autoload.php';
+
+	use Aws\S3\S3Client;
+
+	// Instantiate the S3 client using your Wasabi profile
+	$s3Client = S3Client::factory(array(
+		'endpoint' => 'http://s3.wasabisys.com',
+		'profile' => 'wasabi',
+		'region' => 'eu-central-1',
+		'version' => 'latest',
+	));
+
+	//Sample to create a bucket
+	$s3Client->createBucket(array('Bucket' => 'test1'));
+?>
