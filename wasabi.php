be2f076948d1049adade049d04b8d66b14dc5724
Alexander White <alex@picotory.com>
2021-02-22 23:16:21 +0000

    test

--- wasabi.php
+++ wasabi.php
@@ -1,4 +1,8 @@
 <?php
+	ini_set('display_startup_errors', 1);
+	ini_set('display_errors', 1);
+	error_reporting(-1);
+	
 	// Include the SDK using the Composer autoloader
 	require 'vendor/autoload.php';
 
