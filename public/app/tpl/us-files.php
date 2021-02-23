3aeab6b619f30ca3a9d1dae9e9388860dce09cb5
Alex White <alex@picotory.com>
2021-02-23 12:59:44 +0000

    test custom upload

--- public/app/tpl/us-files.php
+++ public/app/tpl/us-files.php
@@ -389,6 +389,11 @@
 						<div id="filename"></div>
 
 						<form method="post" id="upload" enctype="multipart/form-data" style="margin:0px; padding:0px; display:inline;">
+<div class="mb-3">
+<label for="formFile" class="form-label">Default file input example</label>
+<input class="form-control" type="file" id="formFile">
+</div>
+							
 							<div class="custom-file" ondragover="allowDrop(event)" ondragleave="leaveDrop(event)" id="customFile" style="margin-top:10px;">
 								<input type="file" name="file[]" class="custom-file-input" id="fileInput" aria-describedby="fileHelp" multiple>
 								<label class="custom-file-label" id="custom-file-label" for="fileInput" style="text-align:left;">
