f75c35cdc3c15ae783cf9d883682bfe0b9fd10f5
Alexander White <alex@picotory.com>
2021-02-25 20:25:24 +0000

    dbg

--- public/app/tpl/us-files.php
+++ public/app/tpl/us-files.php
@@ -268,7 +268,7 @@
 										// Get the correct file icon
 										$fIco = StoIco::ShowIcon($file);
 
-										$fileIco = '<i class="' . $fIco . '"></i>';
+										$fileIco = '<i style="font-size: 1.5rem; class="' . $fIco . '"></i>';
 
 										// Encrypt file name
 										$encFile = Storio::SimpleCrypt($usrDir . $getBrowse. '/' . $file);
