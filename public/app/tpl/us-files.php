a9c714f4ed5f53dffbb4678fd887f3a2fe5049f9
Alex White <alex@picotory.com>
2021-02-25 17:28:37 +0000

    arrow

--- public/app/tpl/us-files.php
+++ public/app/tpl/us-files.php
@@ -215,13 +215,8 @@
 
 								// Show the back button if needed
 								if(!empty($_GET['browse'])) {
-									// Arrow icon
-									$arrIco = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-									<path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
-								  </svg>';
-
 									echo '<div class="col-md-12">';
-									echo '<a href="?page=us-files' . Storio::GoBack($_GET['browse']) . '">' . $arrIco . '</a>';
+									echo '<a href="?page=us-files' . Storio::GoBack($_GET['browse']) . '"><i class="fas fa-long-arrow-alt-left"></i></a>';
 									echo '</div>';
 								}
 
