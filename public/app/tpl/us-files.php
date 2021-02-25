a43cdd07f8825938149c77c67f8739c8bfa0ae8d
Alex White <alex@picotory.com>
2021-02-25 17:29:20 +0000

    test

--- public/app/tpl/us-files.php
+++ public/app/tpl/us-files.php
@@ -216,7 +216,7 @@
 								// Show the back button if needed
 								if(!empty($_GET['browse'])) {
 									echo '<div class="col-md-12">';
-									echo '<a href="?page=us-files' . Storio::GoBack($_GET['browse']) . '"><i class="fas fa-long-arrow-alt-left"></i></a>';
+									echo '<a href="?page=us-files' . Storio::GoBack($_GET['browse']) . '"><i class="fas fa-arrow-left"></i></a>';
 									echo '</div>';
 								}
 
