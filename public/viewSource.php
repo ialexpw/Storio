f16c35d5223dbe9bf592dfd3f4c60b1398e83f90
Alexander White <alex@picotory.com>
2021-02-27 12:44:23 +0000

    Image trial

---
+++ public/viewSource.php
@@ -0,0 +1,27 @@
+<?php
+	include 'app/storio.app.php';
+	
+	// Grab the path and user
+	if(isset($_GET['p']) && !empty($_GET['p']) && !isset($_GET['u']) && !empty($_GET['u'])) {
+		// Users directory
+		if(is_dir('../users/' . $_GET['u'])) {
+			// Decrypt the string
+			$usrFile = Storio::SimpleCrypt($_GET['p'], 'd');
+
+			// Check the file exists
+			if(file_exists($usrFile)) {
+				// Is it an image?
+				if(strpos(mime_content_type($usrFile), 'image') !== false) {
+					// Grab the image contents
+					$getImg = file_get_contents($usrFile);
+
+					// Base64 encode it
+					$base64 = 'data:' . mime_content_type($usrFile) . ';base64,' . base64_encode($getImg);
+
+					// Echo the image out
+					echo '<img src="' . $base64 . '" />';
+				}
+			}
+		}
+	}
+?>
