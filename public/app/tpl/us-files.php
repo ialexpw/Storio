72881940341dfaea40f25f42d1ab32b3c8146f47
Alex White <alex@picotory.com>
2021-03-01 16:24:31 +0000

    dbg

--- public/app/tpl/us-files.php
+++ public/app/tpl/us-files.php
@@ -325,7 +325,7 @@
 										if(strpos($mimeType, 'image') !== false) {
 											echo '<div class="col-8 col-md-6" style="margin-bottom:2px;"><a class="noLink" href="#" data-featherlight="viewSource.php?u=' . $_SESSION['Username'] .'&p=' . $encFile .'">' . $fileIco . ' ' . $file . '</a></div>';
 										}else if(strpos($mimeType, 'video') !== false) {
-											echo '<div class="col-8 col-md-6" style="margin-bottom:2px;"><a class="noLink reqBtn" data-bs-toggle="modal" data-bs-target="#exampleModal">' . $fileIco . ' ' . $file . '</a></div>';
+											echo '<div class="col-8 col-md-6" style="margin-bottom:2px;"><a class="noLink reqBtn" data-bs-toggle="modal" data-bs-target="#reqModal">' . $fileIco . ' ' . $file . '</a></div>';
 										}else{
 											echo '<div class="col-8 col-md-6" style="margin-bottom:2px;">' . $fileIco . ' ' . $file . '</div>';
 										}
