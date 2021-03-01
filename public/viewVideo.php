230d577aa4ade4cb496c1a8045b73b976d674297
Alex White <alex@picotory.com>
2021-03-01 17:14:21 +0000

    Test video player

--- public/viewVideo.php
+++ public/viewVideo.php
@@ -18,9 +18,19 @@
 
 	// Grab the path and user
 	if(isset($_GET['vid']) && !empty($_GET['vid'])) {
-		echo '<video class="center" controls>';
-		echo '<source src="viewSource.php?u=' . $_SESSION['Username'] . '&p=' . $_GET['vid'] . '" type="video/mp4">';
-		echo 'Your browser does not support the video tag.';
-		echo '</video>';
+		echo '<script src="https://cdn.plyr.io/3.6.4/plyr.polyfilled.js"></script>';
+		echo '<link rel="stylesheet" href="https://cdn.plyr.io/3.6.4/plyr.css" />';
+
+	echo '<video id="player" playsinline controls>';
+	//echo '<source src="/path/to/video.mp4" type="video/mp4" />';
+	echo '<source src="viewSource.php?u=' . $_SESSION['Username'] . '&p=' . $_GET['vid'] . '" type="video/mp4">';
+	echo '</video>';
+
+
+
+		//echo '<video class="center" controls>';
+		//echo '<source src="viewSource.php?u=' . $_SESSION['Username'] . '&p=' . $_GET['vid'] . '" type="video/mp4">';
+		//echo 'Your browser does not support the video tag.';
+		//echo '</video>';
 	}
 ?>
