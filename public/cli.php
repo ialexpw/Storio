5804df72179d8d94a34743d66a9f9ede6f112b78
Alexander White <alex@picotory.com>
2021-04-12 10:09:32 +0100

    Change password via cli

--- public/cli.php
+++ public/cli.php
@@ -19,7 +19,17 @@
 			if(file_exists('../users/' . $argv[2])) {
 				// Password supplied
 				if(isset($argv[3])) {
-					
+					// Load the user configuration
+					$usrCfg = Storio::UserConfig($argv[2]);
+
+					// Hash the new password
+					$usrCfg['passWord'] = password_hash($argv[3], PASSWORD_DEFAULT);
+
+					// Encode and resave the config
+					$usrCfgEncode = json_encode($usrCfg);
+					file_put_contents('../users/configs/' . $argv[2] . '-cfg.json', $usrCfgEncode);
+
+					exit('Password updated');
 				}else{
 					exit('Supply a password');
 				}
