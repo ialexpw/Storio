e80b89ffd047699a1eb4a92c4cb3d03e7a496d54
Alexander White <alex@picotory.com>
2021-04-14 19:24:57 +0100

    Start adding user function

--- cli.php
+++ cli.php
@@ -14,6 +14,25 @@
 
 	if(php_sapi_name() === 'cli') {
 		if(!empty($argv[1])) {
+			// Create a new user
+			if(strtolower($argv[1]) == 'create-user') {
+				// Check the user exists already
+				if(!file_exists('users/' . $argv[2])) {
+					// Password supplied
+					if(isset($argv[3])) {
+						// Hash the password
+						$usrPass = password_hash($argv[3], PASSWORD_DEFAULT);
+
+						// Create with default perms
+						if(Storio::AddUser(strtolower($argv[2]), $usrPass, 1000, $usrAr, $post['inputEmail'])) {
+
+						}
+					}else{
+						// Random password
+					}
+				}
+			}
+
 			// Set a password
 			if(strtolower($argv[1]) == 'set-password') {
 				// Check the user exists
