77c286433cb461add2b0edbbfbc94f55ce4ca819
Alexander White <alex@picotory.com>
2021-06-16 09:18:24 +0100

    Powered by addition

--- public/app/tpl/us-files.php
+++ public/app/tpl/us-files.php
@@ -422,7 +422,7 @@
 						?>
 					</div>
 				</div>
-				<p class="text-center" style="margin-top:5px;">&copy; Copyright Storio 2021 - <?php echo 'b. ' . shell_exec("git log -1 --pretty=format:'%h'"); ?></p>
+				<p class="text-center" style="margin-top:5px;">Powered by <a href="https://storio.uk">Storio</a> - <?php echo 'b. ' . shell_exec("git log -1 --pretty=format:'%h'"); ?></p>
 			</div>
 		</main>
 
