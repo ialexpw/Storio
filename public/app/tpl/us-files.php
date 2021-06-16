6ad08ac02e62c6f7a469c0590ec0cd40e39de590
Alexander White <alex@picotory.com>
2021-06-16 17:20:42 +0100

    Remove breadcrumb links for now

--- public/app/tpl/us-files.php
+++ public/app/tpl/us-files.php
@@ -245,6 +245,8 @@
 									echo '<a href="?page=us-files' . Storio::GoBack($_GET['browse']) . '"><i class="fas fa-arrow-left"></i></a>';
 									echo '</div>';
 
+									/*
+
 									// Breadcrumbs, remove the first /
 									$makeBread = ltrim($getBrowse, '/');
 									$makeBread = explode("/", $makeBread);
@@ -253,15 +255,6 @@
 									// Store initial variables
 									$hrLink = '';
 									$fullBread = '';
-
-/*
-<nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
-<ol class="breadcrumb">
-<li class="breadcrumb-item"><a href="#">Home</a></li>
-<li class="breadcrumb-item active" aria-current="page">Library</li>
-</ol>
-</nav>
-*/
 
 									// Loop and build the breadcrumbs
 									foreach($makeBread as $breadCrumb) {
@@ -280,6 +273,8 @@
 									// Echo the breadcrumbs
 									echo $fullBread;
 									echo '</div>';
+
+									*/
 								}
 
 								// Check & sort
