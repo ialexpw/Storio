61f239a3d49c68d416c7286c99bccee25cc3484f
Alexander White <alex@picotory.com>
2021-06-18 16:45:23 +0100

    Gold shield

--- public/app/tpl/ad-users.php
+++ public/app/tpl/ad-users.php
@@ -55,12 +55,6 @@
 		<link rel="preconnect" href="https://fonts.gstatic.com">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap">
 
-		<style>
-		.bi-shield {
-			color:yellow;
-		}
-		</style>
-
 		<!-- Custom styles -->
 		<link rel="stylesheet" href="app/css/custom.css">
 	</head>
