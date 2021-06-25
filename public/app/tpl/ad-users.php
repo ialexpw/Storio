784ce432c02fa5a085cd492771b5303276435585
Alexander White <alex@picotory.com>
2021-06-25 14:38:50 +0100

    edit link

--- public/app/tpl/ad-users.php
+++ public/app/tpl/ad-users.php
@@ -245,8 +245,6 @@
 											$usrCfg = json_decode(file_get_contents('../users/configs/' . $usr . '-cfg.json'), true);
 										}
 
-										echo '<a class="noLink editUsr" name="' . $usr . '" href="javascript:;" data-bs-toggle="modal" data-bs-target="#editUser">Test</a>';
-
 										// Add table row
 										echo '<tr>';
 
@@ -255,12 +253,18 @@
 											// Shield icon
 											echo '<td style="width:26%;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shield" viewBox="0 0 16 16">
 													<path d="M5.338 1.59a61.44 61.44 0 0 0-2.837.856.481.481 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.725 10.725 0 0 0 2.287 2.233c.346.244.652.42.893.533.12.057.218.095.293.118a.55.55 0 0 0 .101.025.615.615 0 0 0 .1-.025c.076-.023.174-.061.294-.118.24-.113.547-.29.893-.533a10.726 10.726 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.775 11.775 0 0 1-2.517 2.453 7.159 7.159 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7.158 7.158 0 0 1-1.048-.625 11.777 11.777 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 62.456 62.456 0 0 1 5.072.56z"/>
-													</svg> ' . $usr . '</td>';
+													</svg> ';
+											echo $usr;
+											echo '<a class="noLink float-end editUsr" name="' . $usr . '" href="javascript:;" data-bs-toggle="modal" data-bs-target="#editUser">edit</a>';
+											echo '</td>';
 										}else{
 											// User icon
 											echo '<td style="width:26%;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
 													<path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-													</svg> ' . $usr . '</td>';
+													</svg> ';
+											echo $usr;
+											echo '<a class="noLink float-end editUsr" name="' . $usr . '" href="javascript:;" data-bs-toggle="modal" data-bs-target="#editUser">edit</a>';
+											echo '</td>';
 										}
 
 										// If admin user, storage is not used
@@ -446,10 +450,6 @@
 				</div>
 			</div>
 		</div>
-
-		<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editUser">
-		Launch demo modal
-		</button>
 
 		<!-- Modal for user edit -->
 		<div class="modal fade" id="editUser" tabindex="-1" aria-labelledby="editUserLabel" aria-hidden="true">
