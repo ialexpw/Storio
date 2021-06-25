07d73b4e6d1c99872b87f0c7b45108f4d2b1739a
Alexander White <alex@picotory.com>
2021-06-25 14:43:33 +0100

    Test edit

--- public/editUser.php
+++ public/editUser.php
@@ -1,3 +1,22 @@
 <?php
-	echo 'Hello';
+	/**
+	 * editUser.php
+	 *
+	 * Remotely called file to edit user permissions
+	 *
+	 * @package    Storio
+	 * @author     Alex White
+	 * @copyright  2021 Storio
+	 * @link       https://storio.uk
+	 */
+
+	// Need admin to call this
+	if(!Storio::LoggedIn('admin')) {
+		exit("Permission denied");
+	}
+
+	// Check for the user
+	if(!empty($_GET['uid']) && is_dir('../users/' . $_GET['uid'])) {
+		echo 'Hello ' . $_GET['uid'];
+	}
 ?>
