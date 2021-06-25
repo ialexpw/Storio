de6405cde5c25a2180cc1516b73a5a637bbee782
Alexander White <alex@picotory.com>
2021-06-25 14:54:44 +0100

    Alter form

--- public/app/tpl/ad-users.php
+++ public/app/tpl/ad-users.php
@@ -455,17 +455,13 @@
 		<div class="modal fade" id="editUser" tabindex="-1" aria-labelledby="editUserLabel" aria-hidden="true">
 			<div class="modal-dialog">
 				<div class="modal-content">
-				<div class="modal-header">
-					<h5 class="modal-title" id="editUserLabel">Edit user</h5>
-					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-				</div>
-				<div class="modal-body">
-					<div id="usrDetails"></div>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-					<button type="button" class="btn btn-primary">Save changes</button>
-				</div>
+					<div class="modal-header">
+						<h5 class="modal-title" id="editUserLabel">Edit user</h5>
+						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+					</div>
+					<div class="modal-body">
+						<div id="usrDetails"></div>
+					</div>
 				</div>
 			</div>
 		</div>
