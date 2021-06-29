e89f8da1222d1c243f2fc8875a5485876695dbdd
Alexander White <alex@picotory.com>
2021-06-29 12:22:50 +0100

    test

--- public/testSearch.php
+++ public/testSearch.php
@@ -3,6 +3,7 @@
 	function rglob($pattern, $flags = 0) {
 		$files = glob($pattern, $flags); 
 		foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
+			echo $dir;
 			$files = array_merge($files, rglob($dir.'/'.basename($pattern), $flags));
 		}
 		return $files;
