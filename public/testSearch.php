3c2b69293563d330bc21724c76182b6c9c65d1af
Alexander White <alex@picotory.com>
2021-06-29 10:38:04 +0100

    Test search function

---
+++ public/testSearch.php
@@ -0,0 +1,17 @@
+<?php
+	// Does not support flag GLOB_BRACE
+	function rglob($pattern, $flags = 0) {
+		$files = glob($pattern, $flags); 
+		foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
+			$files = array_merge($files, rglob($dir.'/'.basename($pattern), $flags));
+		}
+		return $files;
+	}
+
+	// usage: to find the test.zip file recursively
+	$result = rglob($_SERVER['DOCUMENT_ROOT'] . '/test.zip');
+	var_dump($result);
+
+	// to find the all files that names ends with test.zip
+	$result = rglob($_SERVER['DOCUMENT_ROOT'] . '/*.zip');
+?>
