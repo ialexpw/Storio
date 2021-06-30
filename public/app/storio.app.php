592a6231fc0b78aa8d94fa7d901cbd2fb318cad1
Alexander White <alex@picotory.com>
2021-06-30 12:50:50 +0100

    Fix to self-call

--- public/app/storio.app.php
+++ public/app/storio.app.php
@@ -479,9 +479,11 @@
 
 		public static function rglob($pattern, $flags = 0) {
 			$files = glob($pattern, $flags);
+
 			foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
-				$files = array_merge($files, rglob($dir.'/'.basename($pattern), $flags));
+				$files = array_merge($files, Storio::rglob($dir.'/'.basename($pattern), $flags));
 			}
+
 			return $files;
 		}
 		
