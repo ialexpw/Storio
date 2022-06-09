7f84e0639ebb1ce28ffee6e62c5bc815a9e51c50
Alexander White <alex@picotory.com>
2022-06-09 16:29:34 +0100

    rm debug

--- public/app/tpl/ad-settings.php
+++ public/app/tpl/ad-settings.php
@@ -236,8 +236,6 @@
 								<div id="passHelp" class="form-text">Make it a good one!</div>
 							</div>
 
-							<?php print_r($usrCfg); ?>
-
 							<!-- Email -->
 							<div class="mb-3">
 								<label for="usrMail" class="form-label">Email</label>
