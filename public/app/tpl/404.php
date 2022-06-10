fd73ea95869db14453af8aded395e4283d7807f2
Alexander White <alex@picotory.com>
2022-06-10 13:56:28 +0100

    Redo 404

--- public/app/tpl/404.php
+++ public/app/tpl/404.php
@@ -14,5 +14,64 @@
 	if(!defined('INC_DATA')) {
 		exit('error');
 	}
+
+	// Load the site configuration
+	$siteCfg = Storio::SiteConfig();
 ?>
-This page or file can not be found. <a href="/?page=login">Return home</a>.
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<title>Storio - Download</title>
+
+		<link rel="canonical" href="">
+
+		<!-- Bootstrap core CSS -->
+		<link rel="stylesheet" href="app/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap">
+
+		<!-- Custom styles -->
+		<link rel="stylesheet" href="app/css/custom.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+			<div class="container-fluid">
+				<i class="bi bi-droplet" style="font-size: 2rem; margin-right:12px; margin-bottom:6px; color: cornflowerblue;"></i>
+				<a class="navbar-brand" href="?page=ad-dashboard"> <?php echo $siteCfg['siteName']; ?></a>
+			</div>
+		</nav>
+
+		<main class="container">
+			<div class="starter-template py-5 px-3">
+				<div class="row">
+					<div class="col-1 col-sm-3"></div>
+
+					<div class="col-10 col-sm-6">
+						<div class="card">
+							<div class="card-body" style="text-align: center;">
+								<!-- 404 Page -->
+								<br />
+								<h4 class="card-title">404</h4><hr>
+
+								This page or file can not be found. <a href="/?page=login">Return home</a>.
+								<br /><br />
+							</div>
+						</div>
+					</div>
+
+					<div class="col-1 col-sm-3"></div>
+				</div>
+				<p class="text-center" style="margin-top:5px;">Powered by <a href="https://github.com/ialexpw/Storio">Storio</a></p>
+			</div>
+		</main>
+
+		<script type="text/javascript" src="app/js/jquery.min.js"></script>
+		<script type="text/javascript" src="app/js/bootstrap.bundle.min.js"></script>
+		<script type="text/javascript" src="app/js/session.js"></script>
+	</body>
+</html>
