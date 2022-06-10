39da8f234a18e74f6889215e08dc1df8fbec58b2
Alexander White <alex@picotory.com>
2022-06-10 11:49:39 +0100

    fix links

--- public/app/tpl/us-files.php
+++ public/app/tpl/us-files.php
@@ -331,15 +331,30 @@
 										// Show actions (download, copy and delete)
 										echo '<div class="col-4 col-md-2" style="text-align:center; margin-bottom:2px;">';
 										
-										echo '<div class="btn-group dropup">';
+										//dropup
+										echo '<div class="btn-group">';
 										echo '<a class="dropdown-toggle" type="button" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">';
 										echo 'Options..';
 										echo '</a>';
 										echo '<ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">';
-										echo '<li><a class="dropdown-item" href="#">Direct Download</a></li>';
-										echo '<li><a class="dropdown-item" href="#">Copy Share Link</a></li>';
-										echo '<li><a class="dropdown-item" href="#">Move File</a></li>';
-										echo '<li><a class="dropdown-item" href="#">Delete File</a></li>';
+										echo '<li><a class="dropdown-item" href="?dl=' . $shareId . '">Direct Download</a></li>';
+
+										// Whether to have the direct download or the download page
+										if($siteCfg['downloadPage']) {
+											echo '<li><a alt="Copy link" class="dropdown-item copyText" id="copyTxt" onClick="showAlert()" data-clipboard-text="' . $webPath . '?page=download&id=' . $shareId . '" href="javascript:;">Copy Share Link</a></li>';
+										}else{
+											echo '<li><a alt="Copy link" class="dropdown-item copyText" id="copyTxt" onClick="showAlert()" data-clipboard-text="' . $webPath . '?dl=' . $shareId . '" href="javascript:;">Copy Share Link</a></li>';
+										}
+										
+										echo '<li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#moveModal" href="#">Move File</a></li>';
+
+										// When deleting a file, ensure we are redirected back
+										if(!empty($_GET['browse'])) {
+											echo '<li><a alt="Delete file" class="dropdown-item delete" href="?page=us-files&browse=' . $_GET['browse'] . '&del=' . $encFile . '&type=file" data-confirm="Are you sure you would like to delete this file?">Delete File</a></li>';
+										}else{
+											echo '<li><a alt="Delete file" class="dropdown-item delete" href="?page=us-files&del=' . $encFile . '&type=file" data-confirm="Are you sure you would like to delete this file?">Delete File</a></li>';
+										}
+										
 										echo '</ul>';
 										echo '</div>';
 
