3ab5ff616c8dd68a9b356eb4a7aa8dc21e0c2473
Alexander White <alex@picotory.com>
2022-06-15 13:57:51 +0100

    test

--- public/app/tpl/download.php
+++ public/app/tpl/download.php
@@ -105,5 +105,54 @@
 		<script type="text/javascript" src="app/js/jquery.min.js"></script>
 		<script type="text/javascript" src="app/js/bootstrap.bundle.min.js"></script>
 		<script type="text/javascript" src="app/js/session.js"></script>
+
+		<!-- Modal for video -->
+		<div class="modal fade" id="reqModal" tabindex="-1" aria-labelledby="reqModalLabel" aria-hidden="true">
+			<div class="modal-dialog modal-xl" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="reqModalLabel">Video Preview</h5>
+						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+					</div>
+					<div class="modal-body" style="margin-bottom:-10px;">
+						<div id="showVid"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<script>
+			// Pop up modal for the video player
+			$(document).ready(function(){
+				// Click the video to preview
+				$('.reqBtn').click(function() {
+					// Store the name
+					var vidSplit = this.name;
+
+					// Split the string
+					vidSplit = vidSplit.split("+Sto+");
+
+					// Generate the iframe link
+					var ifContent = '<iframe style="width:100%; height:650px;" src="viewSource.php?u='+vidSplit[0]+'&p='+vidSplit[1]+'"></iframe>';
+
+					// Ensure html is empty first
+					$('#showVid').html("");
+					$('#reqModal').on('shown.bs.modal', function () {
+						// Load the iframe html in
+						$('#showVid').html(ifContent);
+
+						// Cleanup
+						delete vidSplit;
+						delete ifContent;
+					});
+				});
+
+				// When modal is closed, remove video player (stop sound)
+				$('#reqModal').on('hidden.bs.modal', function () {
+					// Delete the video
+					$('#showVid').html("");
+				});
+			});
+		</script>
 	</body>
 </html>
