c3213e892e3fc70cd84c3e605b921ebc13721b32
Alexander White <alex@picotory.com>
2022-06-30 12:15:54 +0100

    Neaten code

--- public/app/tpl/us-files.php
+++ public/app/tpl/us-files.php
@@ -385,18 +385,17 @@
 							?>
 
 
-<hr>
+							<hr>
 
-<div class="dropdown">
-<button class="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-Group actions
-</button>
-<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-<li><a class="dropdown-item" href="#">Action</a></li>
-<li><a class="dropdown-item" href="#">Another action</a></li>
-<li><a class="dropdown-item" href="#">Something else here</a></li>
-</ul>
-</div>
+							<div class="dropdown">
+								<button class="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+									Group actions
+								</button>
+								<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+									<li><a class="dropdown-item" href="#">Copy Share Link</a></li>
+									<li><a class="dropdown-item" href="#" style="color:indianred;">Delete Selected</a></li>
+								</ul>
+							</div>
 
 						</p>
 					</div>
