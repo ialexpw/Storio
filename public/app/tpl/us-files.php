e3a2e63ebc6f94fe357342997ce15b98feb08057
Alexander White <alex@picotory.com>
2022-06-30 08:51:18 +0100

    test

--- public/app/tpl/us-files.php
+++ public/app/tpl/us-files.php
@@ -383,6 +383,17 @@
 								// End the row
 								echo '</div>';
 							?>
+
+<div class="dropdown">
+<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+Group actions
+</button>
+<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+<a class="dropdown-item" href="#">Action</a>
+<a class="dropdown-item" href="#">Another action</a>
+<a class="dropdown-item" href="#">Something else here</a>
+</div>
+</div>
 						</p>
 					</div>
 				</div>
