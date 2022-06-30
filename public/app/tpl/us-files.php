da232f118c59c97244dda4f7b2f0b3bf0920c4dd
Alexander White <alex@picotory.com>
2022-06-30 08:54:11 +0100

    test button view

--- public/app/tpl/us-files.php
+++ public/app/tpl/us-files.php
@@ -388,7 +388,7 @@
 <hr>
 
 <div class="dropdown">
-<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+<button class="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 Group actions
 </button>
 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
