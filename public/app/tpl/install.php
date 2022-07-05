a5a5f1d27374619adf4509b8367a391f23cb0306
Alexander White <alex@picotory.com>
2022-07-05 15:18:04 +0100

    test form

--- public/app/tpl/install.php
+++ public/app/tpl/install.php
@@ -83,7 +83,7 @@
 										echo $dirCheck;
 									}
 
-									echo '<hr><form method="post">';
+									echo '<hr><form method="post" action="?page=install">';
 									echo '<div class="mb-3">';
 									echo '<label for="uploadPath" class="form-label">Upload path ({user} gets replaced by each user)</label>';
 									echo '<input type="text" class="form-control" id="uploadPath" name="uploadPath" value="../users/{user}">';
