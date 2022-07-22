5c8c274dab4c53ce0351ff071884837675313da0
Alexander White <alex@picotory.com>
2022-07-22 11:50:59 +0100

    Test indentation

--- public/app/tpl/us-files.php
+++ public/app/tpl/us-files.php
@@ -274,7 +274,9 @@
 											</svg>';
 
 										// Folder icon/name
-										echo '<div class="col-8 col-md-8 left-indent" style="margin-bottom:2px;">' . $fol_icon . ' <a href="?page=us-files&browse=' . ltrim($subLink, '/') . '">' . $dir . '</a></div>';
+										echo '<div class="col-8 col-md-8 " style="margin-bottom:2px;">' . $fol_icon . ' <a href="?page=us-files&browse=' . ltrim($subLink, '/') . '">' . $dir . '</a></div>';
+
+										//echo '<div class="col-8 col-md-8 stop-wrap" id="' . $encMultiShare . '-" style="margin-bottom:2px;">';
 
 										echo '<span><input type="checkbox" name="checkBox" value="" style="margin-right:10px;" disabled /></span>';
 
