e62e83b10c90233cd24b560bcdb215c27007c47f
Alexander White <alex@picotory.com>
2022-08-03 10:45:56 +0100

    test

--- public/app/storio.app.php
+++ public/app/storio.app.php
@@ -586,7 +586,7 @@
 			return false;
 		}
 
-		public static function CreateThumb($filepath, $thumbpath, $thumbnail_width, $thumbnail_height, $background=true) {
+		public static function CreateThumb($filepath, $thumbpath, $thumbnail_width, $thumbnail_height, $background='transparent') {
 			list($original_width, $original_height, $original_type) = getimagesize($filepath);
 			if ($original_width > $original_height) {
 				$new_width = $thumbnail_width;
