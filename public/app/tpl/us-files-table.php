2ef80077cc0abbc87212b0507b9c8063f94a3af0
Alexander White <alex@picotory.com>
2022-08-03 11:51:08 +0100

    test folder image

--- public/app/tpl/us-files-table.php
+++ public/app/tpl/us-files-table.php
@@ -268,13 +268,16 @@
 										// Encrypt file name
 										$encFile = Storio::SimpleCrypt($usrDir . $getBrowse. '/' . $dir);
 
+										$folder = file_get_contents('../users/configs/_thumbs/folder.png');
+										$fold_img = 'data:image/png;base64,' . base64_encode($folder);
+
 										// Folder icon
 										$fol_icon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder" viewBox="0 0 16 16">
 											<path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z"/>
 											</svg>';
 
 										// Folder icon/name
-										echo '<div class="col-8 col-md-8" style="margin-bottom:2px;"><span><input type="checkbox" name="checkBox" value="" style="margin-right:10px;" disabled /></span>' . $fol_icon . ' <a href="?page=us-files&browse=' . ltrim($subLink, '/') . '">' . $dir . '</a></div>';
+										echo '<div class="col-8 col-md-8" style="margin-bottom:2px;"><span><input type="checkbox" name="checkBox" value="" style="margin-right:10px;" disabled /></span><img src="' . $fold_img . '"> <a href="?page=us-files&browse=' . ltrim($subLink, '/') . '">' . $dir . '</a></div>';
 
 										// No size shown for directories
 										echo '<div class="col-md-2 d-none d-sm-block" style="margin-bottom:2px;">n/a</div>';
