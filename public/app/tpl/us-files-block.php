a8f40b3373237819aba3dc3c82f11798562465d4
Alexander White <alex@picotory.com>
2022-08-04 15:26:47 +0100

    resize to 150

--- public/app/tpl/us-files-block.php
+++ public/app/tpl/us-files-block.php
@@ -261,7 +261,7 @@
 										$fold_img = 'data:image/png;base64,' . base64_encode($folder);
 
 										echo '<div class="col-md-3" style="text-align:center;">';
-										echo '<a href="?page=us-files-table&browse=' . ltrim($subLink, '/') . '"><img width="200" height="200" src="' . $fold_img . '" class="rounded" alt="..."><br />' . $dir . '</a>';
+										echo '<a href="?page=us-files-table&browse=' . ltrim($subLink, '/') . '"><img width="150" height="150" src="' . $fold_img . '" class="rounded" alt="..."><br />' . $dir . '</a>';
 										echo '</div>';
 /*
 										echo '<tr>';
