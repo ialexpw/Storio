f24b652381434dfe981c405e94f4fa34516017b0
Alexander White <alex@picotory.com>
2022-08-04 15:26:00 +0100

    test

--- public/app/tpl/us-files-block.php
+++ public/app/tpl/us-files-block.php
@@ -261,7 +261,7 @@
 										$fold_img = 'data:image/png;base64,' . base64_encode($folder);
 
 										echo '<div class="col-md-3" style="text-align:center;">';
-										echo '<a style="text-align:center;" href="?page=us-files-table&browse=' . ltrim($subLink, '/') . '"><img style="text-align:center;" width="200" height="200" src="' . $fold_img . '" class="rounded" alt="..." style=""><p style="text-algin:center;">' . $dir . '</p></a>';
+										echo '<a href="?page=us-files-table&browse=' . ltrim($subLink, '/') . '"><img width="200" height="200" src="' . $fold_img . '" class="rounded" alt="..."><p>' . $dir . '</p></a>';
 										echo '</div>';
 /*
 										echo '<tr>';
