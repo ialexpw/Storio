ca1fb5f00a60e879f0ff7198c68aed30fbfd74d2
Alexander White <alex.white@pharmagraph.co.uk>
2022-12-09 10:02:51 +0000

    Fix dropdown

--- public/app/tpl/us-files-table.php
+++ public/app/tpl/us-files-table.php
@@ -368,7 +368,7 @@
 										// Options
 										echo '<td class="text-center">';
 										echo '<div class="btn-group">';
-										echo '<a class="dropdown-toggle" type="button" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">';
+										echo '<a class="" type="button" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">';
 										//echo 'Options..';
 										echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
 										<path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
