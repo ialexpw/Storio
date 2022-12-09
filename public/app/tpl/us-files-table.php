b14adf88dca88b871ad9a1491e7dfbd4c0b06681
Alexander White <alex.white@pharmagraph.co.uk>
2022-12-09 10:05:23 +0000

    Hide size on mobile

--- public/app/tpl/us-files-table.php
+++ public/app/tpl/us-files-table.php
@@ -245,7 +245,7 @@
 								echo '<tr>';
 								echo '<th scope="col" style="width:5%"></th>';
 								echo '<th scope="col" style="width:70%"></th>';
-								echo '<th scope="col" style="width:5%"></th>';
+								echo '<th scope="col" class="fileSize" style="width:5%"></th>';
 								echo '<th scope="col" style="width:20%"></th>';
 								echo '</tr>';
 								echo '</thead>';
@@ -363,13 +363,12 @@
 										}
 
 										// Size
-										echo '<td class="text-center">' . Storio::ReadableSize(filesize($usrDir . $getBrowse. '/' . $file)) . '</td>';
+										echo '<td class="text-center fileSize">' . Storio::ReadableSize(filesize($usrDir . $getBrowse. '/' . $file)) . '</td>';
 
 										// Options
 										echo '<td class="text-center">';
 										echo '<div class="btn-group">';
 										echo '<a class="" type="button" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">';
-										//echo 'Options..';
 										echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
 										<path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
 									  </svg>';
